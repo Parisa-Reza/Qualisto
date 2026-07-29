e5984db446696c9340f01e994949c520c3d556ea
Parisa-Reza <rezaparisa5@gmail.com>
2026-07-29 18:54:56 +0600

    added :  environment variables and PostgreSQL configuration in docker-compose

--- core/settings.py
+++ core/settings.py
@@ -1,17 +1,8 @@
-
-
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-ALLOWED_HOSTS = []
-
-
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+
+ALLOWED_HOSTS = []
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,9 +60,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
